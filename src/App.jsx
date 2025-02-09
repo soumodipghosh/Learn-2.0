679be989cb7cb35da7bf69140c8edbf17260a92b
soumodip ghosh <ghosh.soumodip2002@gmail.com>
2025-02-09 19:21:48 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Feature from './components/Feature'
 import Workflow from './components/Workflow'
--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -8,7 +8,7 @@ const Workflow = () => {
     <div className='mt-20'>
       <h2 className='text-3xl sm:text-5xl lg:text-6xl text-center mt-6 
       tracking-wide'>Accalarate your <span className='bg-gradient-to-r from-orange-500
-        to-orange-800 text-transparent bg-clip-text'>{" "}coding workflow.</span></h2>
+        to-orange-800 text-transparent bg-clip-text'>{""}coding workflow.</span></h2>
         <div className='flex flex-wrap justify-center mt-12'>
             <div className='p-2 w-full lg:w-1/2'><img src={code} alt="" /></div>
             <div className='pt-12 w-full lg:w-1/2'>
